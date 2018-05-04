931b81689b289c05eca63c87b01491a0d6142ac4
Merchandise <t.willys26@gmail.com>
2018-05-04 16:16:16 +0300

    Coding the specifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
-$(document).ready(function)
+var result;
+$(document).ready(function(){
+$('form#pingpong').submit(function(){
+
+});
+});
+
+function React(){
+   for(var counter=1;counter<=result;counter++){
+      if(counter % 15==0){
+        
+      }
+      else if(counter % 5==0){
+
+      }
+      else if(counter % 3==0){
+
+      }
+      else{
+
+      }
+   }
+}
