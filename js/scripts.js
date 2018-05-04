cf2f2b8b8084f7e39e95021d2838702683bb704e
Merchandise <t.willys26@gmail.com>
2018-05-04 16:31:49 +0300

    Coding more of the specifications

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
-var result;
-$(document).ready(function(){
-$('form#pingpong').submit(function(){
+  var result;
+  $(document).ready(function(){
+  $('form#pingpong').submit(function(event){
+  $('ul.data').text('');
+  result=parseInt($('input#txt').val());
+  React();
+  $('input#txt').val('');
+  event.preventDefault();
+  });
+  });
 
-});
-});
-
-function React(){
-   for(var counter=1;counter<=result;counter++){
-      if(counter % 15==0){
-        
-      }
-      else if(counter % 5==0){
-
-      }
-      else if(counter % 3==0){
-
-      }
-      else{
-
-      }
-   }
-}
+  function React(){
+     for(var counter=1;counter<=result;counter++){
+        if(counter % 15==0){
+          $('ul.data').append('<li>pingpong</li>');
+        }
+        else if(counter % 5==0){
+          $('ul.data').append('<li>pong</li>');
+        }
+        else if(counter % 3==0){
+          $('ul.data').append('<li>ping</li>');
+        }
+        else{
+            $('ul.data').append('<li>'+counter+'</li>');
+        }
+     }
+  }
